189d752161926261dd6884d5ca3d4a98b41c01cc
Thomas Linkin <tlinkin@Thomass-MacBook-Air.local>
2012-11-21 10:06:41 -0500

    Initial commit of puppet noop function. This commit contains the initial version of a puppet noop function. This function is designed to set noop to true as a default for all resources in local scope. This setting will be inherited to all children scopes. Any resource can override this default explicitly. This default can be overriden at any child scope for any resource.
    The idea behind this function is to allow a whole class to be set as noop while
    letting other classes still run.

---
+++ lib/parser/functions/noop.rb
@@ -0,0 +1,20 @@
+# Set noop to true as default for current and children scopes
+Puppet::Parser::Functions::newfunction(:noop, :doc => "Set noop default to true for all resources
+  in local scope and children scopes. This can be overriden in 
+  child scopes, or explicitly on each resource.
+  ") do
+
+  class << self
+    def lookupdefaults(type)
+      values = super(type)
+
+      # Create a new :noop paramter with value of true for our defaults hash
+      noop = Puppet::Parser::Resource::Param.new(
+        :name => :noop, :value => 'true', :source => self.source )
+
+      # Replace whatever defaults we recieved 
+      values[:noop] = noop
+      values
+    end
+  end
+end
