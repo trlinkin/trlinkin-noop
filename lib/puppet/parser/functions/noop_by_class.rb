583851c9ec82453530d1276da899d3806525cb74
Thomas Linkin <trlinkin@gmail.com>
2016-05-17 14:03:30 -0700

    Extend `noop_by_class()` prototype
    This commit includes further initial development on the `noop_by_class()`
    method. The improvements include logic to keep from stacking monkey
    patches one on top of the other. This way the "victim scopes" don't keep
    having the original `noop()` method run against them.

--- lib/puppet/parser/functions/noop_by_class.rb
+++ lib/puppet/parser/functions/noop_by_class.rb
@@ -30,21 +30,34 @@ Puppet::Parser::Functions::newfunction(:noop_by_class, :doc => "Set noop default
   # If there are scopes we don't know yet, try to set up to "noop" them when they appear
   topscope = find_global_scope
 
-  (class << topscope; self; end).class_eval do
+  if patch_list = topscope.instance_variable_get(:@_noop_patched)
+    # We already have patched the topscope metaclass, lets just add some class names to the list
+    patch_list = (patch_list + classes).uniq
+    topscope.instance_variable_set(:@_noop_patched, patch_list)
+  else
+    topscope.instance_variable_set(:@_noop_patched, classes)
 
-    old_lookupdefaults = instance_method(:lookupdefaults)
+    (class << topscope; self; end).class_eval do
+      old_lookupdefaults = instance_method(:lookupdefaults)
 
-    define_method(:lookupdefaults) do |type|
-      # For the classes passed as arguments, we will find their scopes and override their lookupdefaults methods
-      classes.each do |klass|
-        puppet_klass = find_hostclass(klass)
-        if puppet_klass && victim_scope = class_scope(puppet_klass)
-          victim_scope.call_function(:noop,[])
+      define_method(:lookupdefaults) do |type|
+        # For the classes passed as arguments, we will find their scopes and override their lookupdefaults methods
+        #classes = self.instance_variable_get(:@_noop_patched)
+        classes = @_noop_patched
+
+        classes.each do |klass|
+          puppet_klass = find_hostclass(klass)
+          if puppet_klass && victim_scope = class_scope(puppet_klass)
+            victim_scope.call_function(:noop,[])
+            @_noop_patched.delete(klass)
+          end
         end
-      end
 
-      values = old_lookupdefaults.bind(self).(type)
-      values
+        # Resume our original lookupdefaults method, already in progress
+        values = old_lookupdefaults.bind(self).(type)
+        values
+      end
     end
   end
 end
+
