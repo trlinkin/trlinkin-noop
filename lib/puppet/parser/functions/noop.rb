4714221a82311a22332b99123e1ea581a68eeb53
Josh <Joshua.small@macquarie.com>
2016-12-19 18:38:37 +1100

    boolean arg to noop() allowing children to divorce their parent's defaults

--- lib/puppet/parser/functions/noop.rb
+++ lib/puppet/parser/functions/noop.rb
@@ -1,16 +1,16 @@
 # Set noop to true as default for current and children scopes
-Puppet::Parser::Functions::newfunction(:noop, :doc => "Set noop default to true for all resources
+Puppet::Parser::Functions::newfunction(:noop, :doc => "Set noop default to true or false for all resources
   in local scope and children scopes. This can be overriden in
   child scopes, or explicitly on each resource.
   ") do |args|
-
+      @noop_value = true if (@noop_value = args[0]).nil?
   class << self
     def lookupdefaults(type)
       values = super(type)
 
       # Create a new :noop paramter with value of true for our defaults hash
       noop = Puppet::Parser::Resource::Param.new(
-        :name => :noop, :value => 'true', :source => self.source )
+        :name => :noop, :value => @noop_value, :source => self.source )
 
       # Replace whatever defaults we recieved
       values[:noop] = noop
--- spec/unit/puppet/parser/functions/noop_spec.rb
+++ spec/unit/puppet/parser/functions/noop_spec.rb
@@ -20,6 +20,17 @@ describe Puppet::Parser::Functions.function(:noop) do
 
     expect { catalog.resource('File[/tmp/foo]')[:noop] }.to be_true
   end
+  
+  it "should give every resource a default of 'noop => false when arg0 is false'" do
+    Puppet[:code] = <<-NOOPCODE
+      noop(false)
+      file {'/tmp/foo':}
+    NOOPCODE
+
+    catalog = scope.compiler.compile
+
+    expect { catalog.resource('File[/tmp/foo]')[:noop] }.to be_false
+  end
 
   it "should give every resource in child scopes a default of 'noop => true'" do
     Puppet[:code] = <<-NOOPCODE
