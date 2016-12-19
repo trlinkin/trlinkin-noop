456a9b81aeac099ce9813f1cf8e93277efe1452e
Josh <Joshua.small@macquarie.com>
2016-12-20 10:36:31 +1100

    doc update

--- lib/puppet/parser/functions/noop.rb
+++ lib/puppet/parser/functions/noop.rb
@@ -1,5 +1,5 @@
-# Set noop to true as default for current and children scopes
-Puppet::Parser::Functions::newfunction(:noop, :doc => "Set noop default to true or false for all resources
+# Set noop to true as default for current and children scopes, or 'reset' an inherited default with noop(false)
+Puppet::Parser::Functions::newfunction(:noop, :doc => "Set noop default for all resources
   in local scope and children scopes. This can be overriden in
   child scopes, or explicitly on each resource.
   ") do |args|
@@ -8,7 +8,7 @@ Puppet::Parser::Functions::newfunction(:noop, :doc => "Set noop default to true 
     def lookupdefaults(type)
       values = super(type)
 
-      # Create a new :noop paramter with value of true for our defaults hash
+      # Create a new :noop parameter with the specified value (true/false) for our defaults hash
       noop = Puppet::Parser::Resource::Param.new(
         :name => :noop, :value => @noop_value, :source => self.source )
 
