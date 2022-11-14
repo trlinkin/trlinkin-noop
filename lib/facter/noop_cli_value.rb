868befa32591fb59ffb4db0a22698d2b44c30480
martyewings <martyewings@gmail.com>
2022-11-14 10:59:42 +0000

    Puppet 7 and PDK template update

--- lib/facter/noop_cli_value.rb
+++ lib/facter/noop_cli_value.rb
@@ -1,17 +1,16 @@
 Facter.add(:noop_cli_value) do
   setcode do
     # This will be nil if Puppet is not available.
-    begin
-      Module.const_get('Puppet')
-    rescue NameError
-      nil
+
+    Module.const_get('Puppet')
+  rescue NameError
+    nil
+  else
+    # if set on the cli, the boolean value. Otherwise undef.
+    if Puppet.settings.set_by_cli?(:noop)
+      Puppet.settings[:noop]
     else
-      # if set on the cli, the boolean value. Otherwise undef.
-      if Puppet.settings.set_by_cli?(:noop)
-        Puppet.settings[:noop]
-      else
-        nil
-      end
+      nil
     end
   end
 end
